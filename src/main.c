2c8e92128632f9a3adae45fe777cddf1a03904ae
realtradam <github@tradam.dev>
2023-03-08 00:20:25 -0500

    Continued rewrite.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,6 @@
 
 #include "rodeo.h"
 
-rodeo_data_p state;
-
 const rodeo_rgba_t red =
 {
 	1.0f, 0.0f, 0.0f,
@@ -48,64 +46,50 @@ const rodeo_rgba_t pink_clear =
 void
 main_loop(void)
 {
-	mrodeo_do(state)
+	mrodeo_frame_do()
 	{
-	   rodeo_rectangle_draw(
-	   state,
-	   100, 100,
-	   50, 50,
-	   red
+		rodeo_rectangle_draw(
+			(rodeo_rectangle_t){ 100, 100, 50, 50 },
+			red
+		);
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 100, 160, 50, 50 },
+	   		green
 	   );
-	   rodeo_rectangle_draw(
-	   state,
-	   100, 160,
-	   50, 50,
-	   green
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 160, 100, 50, 50 },
+	   		blue
 	   );
-	   rodeo_rectangle_draw(
-	   state,
-	   160, 100,
-	   50, 50,
-	   blue
-	   );
-	   rodeo_rectangle_draw(
-	   state,
-	   160, 160,
-	   50, 50,
-	   pink
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 160, 160, 50, 50 },
+	   		pink
 	   );
 
-	   rodeo_rectangle_draw(
-	   state,
-	   300, 300,
-	   50, 50,
-	   red_clear
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 300, 300, 50, 50 },
+	   		red_clear
 	   );
-	   rodeo_rectangle_draw(
-	   state,
-	   310, 310,
-	   50, 50,
-	   green_clear
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 310, 310, 50, 50 },
+	   		green_clear
 	   );
-	   rodeo_rectangle_draw(
-	   state,
-	   320, 320,
-	   50, 50,
-	   blue_clear
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 320, 320, 50, 50 },
+	   		blue_clear
 	   );
-	   rodeo_rectangle_draw(
-	   state,
-	   330, 330,
-	   50, 50,
-	   pink_clear
+	   rodeo_rectangle_draw(  
+	   		(rodeo_rectangle_t){ 330, 330, 50, 50 },
+	   		pink_clear
 	   );
 
-	rodeo_debug_text_draw(
+		rodeo_debug_text_draw(
 			1,
 			1,
 			" using %s renderer ",
-			rodeo_renderer_name_get()
-			);
+			rodeo_string_to_constcstr(
+				rodeo_renderer_name_get()
+			)
+		);
 	}
 }
 
@@ -113,16 +97,14 @@ int
 main()
 {
 	printf("Init window...\n");
-	mrodeo_window_do(state, 480, 640, "Rodeo Window")
+	mrodeo_window_do(480, 640, "Rodeo Window")
 	{
 		printf("done creating window\n");
 
-		rodeo_mainloop_set(
-				state,
-				main_loop
-				);
+		rodeo_mainloop_run(
+			main_loop
+		);
 
 	}
-	rodeo_deinit();
 	return 0;
 }
